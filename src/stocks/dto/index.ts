da825bff11f50a46275f7bc8b8a9464f613b2c3b
Caio Carnelos <caio.emidio@gmail.com>
2021-03-17 03:46:02 +0000

    Fix Wallets

--- src/stocks/dto/index.ts
+++ src/stocks/dto/index.ts
@@ -1,2 +1,3 @@
-export * from "./create-stock-user.dto"
-export * from "./update-stock-user.dto"
+export * from './create-stock-user.dto'
+export * from './update-stock-user.dto'
+export * from './delete-stock.dto'
--- src/wallets/wallets.service.ts
+++ src/wallets/wallets.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { ReturnModelType } from '@typegoose/typegoose';
+import { StocksService } from 'src/stocks/stocks.service';
 import { CreateWalletDTO } from './dto/create-wallet.dto';
 import { UpdateWalletDTO } from './dto/update-wallet.dto';
 import { Wallets, WalletsFeatureProvider } from './schemas/wallets.schema';
@@ -8,6 +9,8 @@ import { Wallets, WalletsFeatureProvider } from './schemas/wallets.schema';
 @Injectable()
 export class WalletsService {
   constructor(
+    @Inject(forwardRef(() => StocksService))
+    private stocksService: StocksService,
     @InjectModel(WalletsFeatureProvider.name)
     private readonly walletsModel: ReturnModelType<typeof Wallets>,
   ) {}
@@ -27,9 +30,14 @@ export class WalletsService {
   public async find(username: string) {
     const owner = await this.walletsModel
       .find({ ownerUsername: username })
-      .lean();
+      .exec();
     // const shared = await this.walletsModel.find({ sharedUsers: {$in: });
-    return { ...owner };
+    const result = [];
+    for (const wallet of owner) {
+      const consolidated = await this.stocksService.consolidated(wallet._id.toString());
+      result.push({ ...wallet._doc, ...consolidated });
+    }
+    return { ...result };
   }
 
   public async update(dto: UpdateWalletDTO) {
